68f083faf62fcd5b222214a7189aad8a25e56ff5
Maestus <xynian@outlook.com>
2023-07-18 11:43:37 +0300

    core code is finished

--- dataScrapper.py
+++ dataScrapper.py
@@ -11,7 +11,7 @@ import seaborn as sns #install seaborn
 
 #ia = Cinemagoer() #not going use it further
 
-pages = np.arange(1, 2, 50) #entry (start, stop), lines between each entry 
+pages = np.arange(1, 100, 50) #entry (start, stop), lines between each entry 
 headers = {'Accept-Language': 'en-US,en;q=0.8'} # the default language is mandarin
 
 #initialize empty lists to store the variables scraped
@@ -33,8 +33,82 @@ for page in pages:
         + str(page)
         + "&explore=title_type,genres&ref_=adv_prv", headers=headers)
   
-    sleep(randint(8,15)) #probably exists for rate limit
+    #probably exists for rate limit
+    sleep(randint(8,15)) 
 
-      #throw warning for status codes that are not 200
+    #throw warning for status codes that are not 200
     if response.status_code != 200:
        warn('Request: {}; Status code: {}'.format(requests, response.status_code))
+    
+    #parse the content of current iteration of request
+    page_html = BeautifulSoup(response.text, 'html.parser')
+      
+    movie_containers = page_html.find_all('div', class_ = 'lister-item mode-advanced')
+
+    #extract the 50 movies for that page
+    for container in movie_containers:
+       
+        #conditional for all with metascore
+       if container.find('div', class_ = 'ratings-metascore') is not None:
+
+           #title
+            title = container.h3.a.text
+            titles.append(title)
+
+            if container.h3.find('span', class_= 'lister-item-year text-muted unbold') is not None:
+            
+                #year released
+                year = container.h3.find('span', class_= 'lister-item-year text-muted unbold').text # remove the parentheses around the year and make it an integer
+                years.append(year)
+
+            else:
+                years.append(None) # each of the additional if clauses are to handle type None data, replacing it with an empty string so the arrays are of the same length at the end of the scraping
+
+            if container.p.find('span', class_ = 'certificate') is not None:
+                #rating
+                rating = container.p.find('span', class_= 'certificate').text
+                ratings.append(rating)
+
+            else:
+                ratings.append("")
+
+            if container.p.find('span', class_ = 'genre') is not None:
+                #genre
+                genre = container.p.find('span', class_ = 'genre').text.replace("\n", "").rstrip().split(',') # remove the whitespace character, strip, and split to create an array of genres
+                genres.append(genre)
+          
+            else:
+                genres.append("")
+
+            if container.p.find('span', class_ = 'runtime') is not None:
+
+                #runtime
+                time = int(container.p.find('span', class_ = 'runtime').text.replace(" min", "")) # remove the minute word from the runtime and make it an integer
+                runtimes.append(time)
+
+            else:
+                runtimes.append(None)
+
+            if float(container.strong.text) is not None:
+                #IMDB ratings
+                imdb = float(container.strong.text) # non-standardized variable
+                imdb_ratings.append(imdb)
+
+            else:
+                imdb_ratings.append(None)
+
+            if container.find('span', class_ = 'metascore').text is not None:
+                #Metascore
+                m_score = int(container.find('span', class_ = 'metascore').text) # make it an integer
+                metascores.append(m_score)
+
+            else:
+                metascores.append(None)
+
+            if container.find('span', attrs = {'name':'nv'})['data-value'] is not None:
+                #Number of votes
+                vote = int(container.find('span', attrs = {'name':'nv'})['data-value'])
+                votes.append(vote)
+
+            else:
+                votes.append(None)
