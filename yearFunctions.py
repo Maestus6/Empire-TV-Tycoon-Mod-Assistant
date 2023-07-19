76195a0a21358b4e3da0775a3af8d1675b7ad540
Maestus <xynian@outlook.com>
2023-07-19 21:16:02 +0300

    Fixed yearFormatter, and location of launch.json

--- yearFunctions.py
+++ yearFunctions.py
@@ -1,5 +1,10 @@
 def yearFormatter(year):
-    year = year.replace('(','').replace(')','').replace('-','').replace('�','').replace(' ','') 
-    return year
+    year = year.replace('(','').replace(')','').replace('-','').replace('�','').replace(' ','') # doesn't fixes the problem for special invis characters
+
+    purifiedYear = []
+    for index in range(0, len(year), 4):
+        purifiedYear.append(year[index:index+4])
+    
+    return purifiedYear[0]
 
     
