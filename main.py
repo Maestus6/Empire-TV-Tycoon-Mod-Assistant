21aef383a4629d0d30adff30dab90d60e4d0118a
Maestus <xynian@outlook.com>
2023-07-20 10:30:30 +0300

    Fixed gitignore and updated tasks available

--- main.py
+++ main.py
@@ -41,16 +41,17 @@ movieorseries = []
 #!!!-------------------------------------!!!!!!!!!!-------------------------------------!!!#
 
 #THINGS NEEDS TO BE DONE, ORDERED BY IMPORTANCE
+#0- Add an control mechanism to prevent unaired yet added on IMDb to getting processed to prevent errors (NEEDED) 
 #1- Banners come in really small sizes, need to find alternative sources to extract them from (NEEDED)
 #2- Find a way to get Storylines - Movie Descs. It is placed in a really generic place at movie containers to be extracted properly (NEEDED)
 #3- Find a place to get a wide picture of a movie to put it on TV Screen, and do necessary changes in the code to implent it (NEEDED)
 #4- Find a way to differentiate  Anime from Animations. It is written at some place of the movie pages if I recall correctly, need to find it (NEEDED)
 #5- Organize the code, and make things put in order. Fit it into an acceptable structure. (NEEDED)
-#6- Find a way to only take movies between exact dates, as getting new movies with no start dates etc. 
-# breaks everything and we still need that function even if it wasnt the case. (NEEDED)
+#6- Find a way to only take movies between exact dates for get Request, as getting new movies with no start dates etc. (NEEDED)
 #7- A minor task, needs to add rating and genre filter functions for TV-Series. (NEEDED)
 #8- Get data from quote webpages to fill in Quote/Viewer Speech part of the XML, it is not needed but would be amazing (OPTIONAL)
 #9- Find a way to determine GLAMOUR(For females) series. It might be easier to said that done and not absolutely needed. (OPTIONAL)
+#10 Format every variable and function name into camelCase (NEEDED)
 
 #!!!-------------------------------------!!!!!!!!!!-------------------------------------!!!#
 
@@ -77,7 +78,7 @@ for page in pages:
     #extract the 50 movies for that page
     for container in movie_containers:
 
-        #print(container)
+        print(container)
         #title
         title = container.h3.a.text
         titles.append(title)
